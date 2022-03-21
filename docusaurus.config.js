d556d0a13db27a71a008f314a267852bd0d129aa
Posandu Mapa <posandumapa@gmail.com>
2022-03-21 19:09:40 +0530

    fixes design

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,116 +6,106 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Win11React",
-  url: "https://win11docs.asylum-os.com",
-  baseUrl: "/",
-  tagline: "Windows 11 in React",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "favicon.ico",
-  organizationName: "win11react", // Usually your GitHub org/user name.
-  projectName: "docs", // Usually your repo name.
+	title: "Win11React",
+	url: "https://win11docs.asylum-os.com",
+	baseUrl: "/",
+	tagline: "Windows 11 in React",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "favicon.ico",
+	organizationName: "win11react", // Usually your GitHub org/user name.
+	projectName: "docs", // Usually your repo name.
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          editUrl: "https://github.com/win11react/docs/edit/main/",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: "https://github.com/win11react/docs/edit/main/blog/",
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			"classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve("./sidebars.js"),
+					// Please change this to your repo.
+					editUrl: "https://github.com/win11react/docs/edit/main/",
+				},
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /**  @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    {
-      colorMode: {
-        respectPrefersColorScheme: true,
-      },
-      navbar: {
-        title: "Win11React",
-        logo: {
-          alt: "Win11React Logo",
-          src: "img/logo.png",
-        },
-        items: [
-          {
-            type: "doc",
-            docId: "about",
-            position: "left",
-            label: "Docs",
-          },
-          { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://win11.blueedge.me/",
-            label: "Win11React",
-            position: "right",
-          },
-        ],
-        hideOnScroll: true,
-      },
-      footer: {
-        style: "light",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/about",
-              },
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Discord",
-                href: "https://discord.gg/qmEZwUhb4b",
-              },
-              {
-                label: "Github",
-                href: "https://github.com/blueedgetechno/win11React",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Win11React",
-                href: "https://win11.blueedge.me/",
-              },
-              {
-                label: "Status",
-                href: "https://win11react.github.io/status/",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Bluedge. Built with ReactJS.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    },
+	themeConfig:
+		/**  @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		{
+			colorMode: {
+				respectPrefersColorScheme: true,
+			},
+			navbar: {
+				title: "Win11React",
+				logo: {
+					alt: "Win11React Logo",
+					src: "img/logo.png",
+				},
+				items: [
+					{
+						type: "doc",
+						docId: "about",
+						position: "left",
+						label: "Docs",
+					},
+					{
+						href: "https://win11.blueedge.me/",
+						label: "Win11React",
+						position: "right",
+					},
+				],
+				hideOnScroll: true,
+			},
+			footer: {
+				style: "light",
+				links: [
+					{
+						title: "Docs",
+						items: [
+							{
+								label: "Tutorial",
+								to: "/docs/about",
+							},
+						],
+					},
+					{
+						title: "Community",
+						items: [
+							{
+								label: "Discord",
+								href: "https://discord.gg/qmEZwUhb4b",
+							},
+							{
+								label: "Github",
+								href: "https://github.com/blueedgetechno/win11React",
+							},
+						],
+					},
+					{
+						title: "More",
+						items: [
+							{
+								label: "Win11React",
+								href: "https://win11.blueedge.me/",
+							},
+							{
+								label: "Status",
+								href: "https://win11react.github.io/status/",
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Blue edge.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		},
 };
 
 module.exports = config;
